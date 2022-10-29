b2bbfe7b32faf61e9fd1fc5a17edaf2643ec44b7
mao <1296193245@qq.com>
2022-10-29 13:05:22 +0800

    hibernate_validator_demo

---
+++ src/main/java/mao/hibernate_validator_demo/controller/UserController.java
@@ -0,0 +1,46 @@
+package mao.hibernate_validator_demo.controller;
+
+import mao.hibernate_validator_demo.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Project name(项目名称)：hibernate_validator_demo
+ * Package(包名): mao.hibernate_validator_demo.controller
+ * Class(类名): UserController
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/10/29
+ * Time(创建时间)： 13:01
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@RestController
+//开启校验
+@Validated
+public class UserController
+{
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping("/delete")
+    public String delete(@NotBlank(message = "id不能为空") String id)
+    {
+        log.info("delete..." + id);
+        return "OK";
+    }
+
+    @RequestMapping("/save")
+    public String save(@Validated @RequestBody User user)
+    {
+        log.info("save..." + user);
+        return "OK";
+    }
+}
