10546977e44cb606ff308b1e5f769aabd2993441
mao <1296193245@qq.com>
2022-10-29 13:21:24 +0800

    hibernate_validator_demo

---
+++ src/main/java/mao/hibernate_validator_demo/config/ValidatorConfig.java
@@ -0,0 +1,22 @@
+package mao.hibernate_validator_demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Project name(项目名称)：hibernate_validator_demo
+ * Package(包名): mao.hibernate_validator_demo.config
+ * Class(类名): ValidatorConfig
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/10/29
+ * Time(创建时间)： 13:21
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@Configuration
+public class ValidatorConfig
+{
+
+}
