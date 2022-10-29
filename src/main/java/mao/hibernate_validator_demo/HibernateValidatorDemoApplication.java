c78a197e952f619202066793f84ba0f71e114469
mao <1296193245@qq.com>
2022-10-29 13:26:00 +0800

    hibernate_validator_demo

--- src/main/java/mao/hibernate_validator_demo/HibernateValidatorDemoApplication.java
+++ src/main/java/mao/hibernate_validator_demo/HibernateValidatorDemoApplication.java
@@ -1,9 +1,11 @@
 package mao.hibernate_validator_demo;
 
+import mao.hibernate_validator_demo.config.EnableFormValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFormValidator
 public class HibernateValidatorDemoApplication
 {
 
--- src/main/java/mao/hibernate_validator_demo/config/ValidatorConfig.java
+++ src/main/java/mao/hibernate_validator_demo/config/ValidatorConfig.java
@@ -1,6 +1,12 @@
 package mao.hibernate_validator_demo.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 /**
  * Project name(项目名称)：hibernate_validator_demo
@@ -15,8 +21,32 @@ import org.springframework.context.annotation.Configuration;
  * Description(描述)： 无
  */
 
-@Configuration
+
 public class ValidatorConfig
 {
+    @Bean
+    public Validator validator()
+    {
+        ValidatorFactory validatorFactory =
+                Validation.byProvider(HibernateValidator.class)
+                        .configure()
+                        //快速失败返回模式
+                        .addProperty("hibernate.validator.fail_fast", "true")
+                        .buildValidatorFactory();
+        return validatorFactory.getValidator();
+    }
 
+    /**
+     * 开启快速返回
+     * 如果参数校验有异常，直接抛异常，不会进入到 controller，使用全局异常拦截进行拦截
+     */
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor()
+    {
+        MethodValidationPostProcessor postProcessor =
+                new MethodValidationPostProcessor();
+        /*设置validator模式为快速失败返回*/
+        postProcessor.setValidator(validator());
+        return postProcessor;
+    }
 }
